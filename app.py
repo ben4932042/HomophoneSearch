6e1aa1bf2d64bde28782cbe15bdc657baaae097d
Ben Liu <63529371+ben4932042@users.noreply.github.com>
2021-08-02 08:54:54 +0800

    Implement Flask API

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from HomophoneSearch.Chinese.transfer import ChineseTransfer
+from HomophoneSearch.English.transfer import EnglishTransfer
+
+app = Flask(__name__)
+
+
+@app.route('/HomophoneSearch')
+def homephone():
+    word = request.args.get('word')
+    language = request.args.get('language')
+    if language == 'chinese':
+        return_list = ChineseTransfer.text_to_text(str(word))
+    elif language == 'english':
+        return_list = EnglishTransfer.text_to_text(str(word))
+    else:
+        raise
+    return jsonify({'language': language, 'text': return_list})
+
+if __name__ == "__main__":
+    app.run()
